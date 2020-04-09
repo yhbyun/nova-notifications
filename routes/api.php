fea815df6de7caee1e2ddcc6ff553934ba853da3
YongHun Byun <yonghunbyun@gmail.com>
2020-04-09 15:08:25 +0900

    Fix CS

--- routes/api.php
+++ routes/api.php
@@ -5,39 +5,38 @@ use Illuminate\Notifications\DatabaseNotification;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
- Route::get('/unread', function (Request $request) {
-     $notifications = $request->user()->unreadNotifications->mapWithKeys(function (DatabaseNotification $notification) {
-         return [$notification->id => $notification];
-     });
+Route::get('/unread', function (Request $request) {
+    $notifications = $request->user()->unreadNotifications->mapWithKeys(function (DatabaseNotification $notification) {
+        return [$notification->id => $notification];
+    });
 
-     return Response::json([
-         'count' => $notifications->count(),
-         'notifications' => $notifications,
-         'user_id' => $request->user()->id,
-     ]);
- });
+    return Response::json([
+        'count' => $notifications->count(),
+        'notifications' => $notifications,
+        'user_id' => $request->user()->id,
+    ]);
+});
 
+Route::patch('/{notification}', function (Request $request, $notification) {
+    $markRead = $request
+        ->user()
+        ->unreadNotifications()
+        ->find($notification)
+        ->markAsRead();
 
- Route::patch('/{notification}', function (Request $request, $notification) {
-     $markRead = $request
-         ->user()
-         ->unreadNotifications()
-         ->find($notification)
-         ->markAsRead();
+    return Response::json([
+        'notification' => $request
+            ->user()
+            ->notifications()
+            ->find($notification),
+    ]);
+});
 
-     return Response::json([
-         'notification' => $request
-             ->user()
-             ->notifications()
-             ->find($notification),
-     ]);
- });
-
- Route::patch('/', function (Request $request) {
-     return Response::json([
-         'status' => $request
-             ->user()
-             ->unreadNotifications
-             ->markAsRead(),
-     ]);
- });
+Route::patch('/', function (Request $request) {
+    return Response::json([
+        'status' => $request
+            ->user()
+            ->unreadNotifications
+            ->markAsRead(),
+    ]);
+});
